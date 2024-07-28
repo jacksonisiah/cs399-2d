c9deac1d881a0a78fa29c99605ac6b60ccfb9a0a
Jackson <jackson@catfile.me>
2024-07-28 00:37:25 -0400

    Add audio tracks and cutscene

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,11 +1,49 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
+    public AudioClip buttonClickSFX;
+    public AudioClip buttonSelectSFX;
+    [SerializeField]
+    private AudioSource cameraAudioSource;
+    [SerializeField]
+    private AudioSource menuAudioSource;
+    public Slider sldMusicVolume;
+    public Toggle sfxToggle;
+    
    public void LoadLevel() => 
       SceneManager.LoadScene("Level1");
+
+   public void Start()
+   {
+       menuAudioSource = GetComponent<AudioSource>();
+       cameraAudioSource = Camera.main!.GetComponent<AudioSource>();
+
+       sldMusicVolume.value = cameraAudioSource.volume;
+       menuAudioSource.PlayOneShot(buttonClickSFX);
+
+       sfxToggle.isOn = Configs.SFX;
+   }
+
+   public void OnVolumeChange()
+   {
+       cameraAudioSource.volume = sldMusicVolume.value;
+   }
+
+   public void OnButtonClick()
+   {
+       if (Configs.SFX)
+           menuAudioSource.PlayOneShot(buttonClickSFX);
+   }
+
+   public void OnButtonSelect()
+   {
+       if (Configs.SFX)
+        menuAudioSource.PlayOneShot(buttonSelectSFX);
+   }
 
    public void Exit()
    {
@@ -14,5 +52,10 @@ public class TitleScreen : MonoBehaviour
 #else
       Application.Quit();
 #endif 
+   }
+
+   public void OnSFXToggle()
+   {
+       Configs.SFX = !Configs.SFX;
    }
 }
